50d40e59a61fbc4c0101297462a0938d5f5ff8e7
Adam Achebe <mister363636@gmail.com>
2022-06-15 20:13:53 -0500

    Created interface

--- contracts/AMZ.sol
+++ contracts/AMZ.sol
@@ -2,8 +2,34 @@
 pragma solidity ^0.8.14;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "./stringUtils.sol";
+
+interface IGiftCard is IERC721 {
+    function mintCard(
+        address to,
+        uint256 balance,
+        string calldata claimCode
+    ) external returns (uint256);
+
+    function checkIfCardIsMinted(string calldata claimCode)
+        external
+        view
+        returns (bool);
+
+    function getBalance(uint256 tokenId) external view returns (uint256);
+
+    function getClaimCode(uint256 tokenId) external returns (string memory);
+
+    function getOriginalMinter(uint256 tokenId) external view returns (address);
+
+    function isCodeApplied(uint256 tokenId) external view returns (bool);
+
+    function changeBalance(uint256 newBalance, uint256 tokenId)
+        external
+        returns (uint256);
+}
 
 contract GiftCard is ERC721 {
     using Counters for Counters.Counter;
